47d1aa17cf02d84c1c21e6e9bb882467dbf5f293
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2025-08-20 22:50:02 +0300

    added

---
+++ OrderManagementAPI/Migrations/20250820194840_FinalStructure.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalStructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
